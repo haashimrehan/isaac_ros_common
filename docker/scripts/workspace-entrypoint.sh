e6664cd92497cd60956c5939677d9ebb5cb26f3d
Haashim Rehan <haashimr@hotmail.com>
2023-06-20 02:04:49 -0400

    added rosinstall for external packages

--- docker/scripts/workspace-entrypoint.sh
+++ docker/scripts/workspace-entrypoint.sh
@@ -9,11 +9,26 @@
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 
 # Build ROS dependency
+echo "alias build_workspace='cd /workspaces/isaac_ros-dev && colcon build --symlink-install && source install/setup.bash'" >> ~/.bashrc
 echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
 sudo apt-get update
 rosdep update
+
+# Install ROS2 deps
+source ${ROS_ROOT}/install/setup.bash \
+    && export ROS_PACKAGE_PATH=${AMENT_PREFIX_PATH} \
+    && mkdir -p src/external_packages \
+    && rosinstall_generator --deps --exclude RPP --rosdistro ${ROS_DISTRO} \
+    `rosdep keys --ignore-src --rosdistro=humble --from-paths src | egrep -v "PKG_2_SKIP1|PKG_2_SKIP2"` \
+    > ros.${ROS_DISTRO}.external_packages.rosinstall \
+    && cat ros.${ROS_DISTRO}.external_packages.rosinstall \
+    && vcs import src/external_packages < ros.${ROS_DISTRO}.external_packages.rosinstall 
+
+source ${ROS_ROOT}/install/setup.bash && apt-get update || true && rosdep update && rosdep install -y --skip-keys="PUT_DEPS_TO_SKIP_HERE" \
+    --from-paths src --ignore-src -y --rosdistro=${ROS_DISTRO}
+
 
 # Restart udev daemon
 sudo service udev restart
